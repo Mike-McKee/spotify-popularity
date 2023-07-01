97b9d0061c9b7e763764569e9b3909525f42c67f
MIchael McKee <mike@dorkydata.com>
2023-07-01 09:49:15 -0400

    Successfully got artist data

--- get_artists.py
+++ get_artists.py
@@ -13,16 +13,13 @@ client_secret = secret_id.CLIENT_SECRET
 cc_manager = scc(client_id=client_id, client_secret=client_secret)
 spotify = spotipy.Spotify(client_credentials_manager=cc_manager)
 
-with open('artists.csv', 'r+') as file:
+with open('artists.csv', 'r') as file:
 	artists = csv.reader(file)
-
-	new_headers = ['Artist_Popularity', 'Top_Track_1', 'Top_Track_2', 'Top_Track_3',
-				  'Top_Track_4', 'Top_Track_5', 'Top_Track_6', 'Top_Track_7', 
-				  'Top_Track_8', 'Top_Track_9', 'Top_Track_10']
 	
-	header_row = next(artists)
-	header_row.extend(new_headers)
+	#skips the first row
+	next(artists)
 
+	artist_data = []
 	for row in artists:
 		artist = spotify.artist(row[1])
 		tracks = spotify.artist_top_tracks(row[1])
@@ -34,20 +31,27 @@ with open('artists.csv', 'r+') as file:
 		for i in list_t:
 			track = i['name']
 			t_tracks.append(track)
-		
-		row.extend([artist_pop] + t_tracks)
 
-	file.seek(0)
-	writer = csv.writer(file)
+		new_row = row + [artist_pop] + [t for t in t_tracks] 
+		artist_data.append(new_row)
+
+with open('updated_artists.csv', 'w') as new_file:
+	writer = csv.writer(new_file)
+	
+	header_row = ['Artist_Name', 'Artist_Id', 'Artist_Popularity', 'Top_Track_1', 'Top_Track_2', 'Top_Track_3',
+				  'Top_Track_4', 'Top_Track_5', 'Top_Track_6', 'Top_Track_7', 
+				  'Top_Track_8', 'Top_Track_9', 'Top_Track_10']
+	
 	writer.writerow(header_row)
-	writer.writerow(artists)
+	for k in artist_data:
+		writer.writerow(k)
 
+print("=====================SUCCESS=====================")
 
-"""
-So close...
+#Had some fun and tested how long this program takes to run.... Results below
+end = time.time()
+runtime = end - start
+print(f"This program took {runtime} seconds to successfully run")
 
-We got the code to work and write in the csv, but it added all values to the end of the
-csv as one GIANT list.
-
-So we've gotta find a way to rewrite the file instead of just appending to it.
-"""
+#First attempt took 73.03422427177429 seconds
+#Second attempt took 81.47556614875793 seconds
