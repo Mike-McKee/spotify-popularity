aff0fd6eb2c07cdd12d12f9d2c0586fb7da6f071
MIchael McKee <mike@dorkydata.com>
2023-06-30 21:18:41 -0400

    working on getting artist data

--- get_artists.py
+++ get_artists.py
@@ -1,21 +1,11 @@
 #Code to get the artists data using Spotify's API
-
-
-"""
-what we need to do:
-1. create list of artists names by iterating through playlist.csv and creating
-list of all artists from 1-6 (be sure to not duplicate info... and save their 
-names and id numbers)
-2. create while loop to keep requesting data (and appending to an empty list) until
-we have the info for every artist on the list)
-3. While we're doing this, we might as well get the artists top tracks at the same
-time using a separate request
-"""
 import secret_id
-import json
+import time
 import csv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials as scc
+
+start = time.time()
 
 client_id = secret_id.CLIENT_ID
 client_secret = secret_id.CLIENT_SECRET
@@ -23,45 +13,41 @@ client_secret = secret_id.CLIENT_SECRET
 cc_manager = scc(client_id=client_id, client_secret=client_secret)
 spotify = spotipy.Spotify(client_credentials_manager=cc_manager)
 
-def iterate_csv(file_name, column_name):
-	with open(file_name, 'r') as file:
-		playlist = csv.reader(file, delimiter=',')
-		for row in playlist:
-			value = row[column_name]
-			return value
+with open('artists.csv', 'r+') as file:
+	artists = csv.reader(file)
 
-def get_artists(file_name):
-	name_headers = ['Artist_1_Name', 'Artist_2_Name', 'Artist_3_Name', 
-									'Artist_4_Name', 'Artist_5_Name', 'Artist_6_Name']
-	id_headers = ['Artist_1_Popularity', 'Artist_2_Popularity', 'Artist_3_Popularity',
-		            'Artist_4_Popularity', 'Artist_5_Popularity', 'Artist_6_Popularity']
-	artist_values = {}
-	for i in name_headers:
-		iterate_csv(file_name, i)
-		artist_values[i] = id_headers[i]
+	new_headers = ['Artist_Popularity', 'Top_Track_1', 'Top_Track_2', 'Top_Track_3',
+				  'Top_Track_4', 'Top_Track_5', 'Top_Track_6', 'Top_Track_7', 
+				  'Top_Track_8', 'Top_Track_9', 'Top_Track_10']
 	
-	return artists_values
+	header_row = next(artists)
+	header_row.extend(new_headers)
 
-#print(get_artists())
+	for row in artists:
+		artist = spotify.artist(row[1])
+		tracks = spotify.artist_top_tracks(row[1])
 
-def main(file):
-	with open('name_csv.csv', 'w', newline='', encoding='utf-7') as new_file:
-		writer = csv.writer(new_file)
-
-		headers = ['Artist_Name', 'Artist_Id']
-		writer.writerow(headers)
-		writer.writerows(get_artists(file))
-
-		print("Success")
-
-main('playlist.csv')
-
-#So far this code fails... need to work on it
+		artist_pop = artist['popularity']
 		
+		list_t = tracks['tracks']
+		t_tracks = []
+		for i in list_t:
+			track = i['name']
+			t_tracks.append(track)
+		
+		row.extend([artist_pop] + t_tracks)
+
+	file.seek(0)
+	writer = csv.writer(file)
+	writer.writerow(header_row)
+	writer.writerow(artists)
 
 
+"""
+So close...
 
+We got the code to work and write in the csv, but it added all values to the end of the
+csv as one GIANT list.
 
-
-
-
+So we've gotta find a way to rewrite the file instead of just appending to it.
+"""
